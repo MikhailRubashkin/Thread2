c28714deb4c249b9c3dddfb380a088b89ee2a3d0
MikhailRubashkin <pristijpribor@mail.ru>
2019-06-14 18:12:27 +0300

    not is ok

---
+++ src/AssistantScientist.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class AssistantScientist {
+
+    private int minNumberPartsReturned;
+    private int maxNumberPartsReturned;
+
+    public AssistantScientist(int minNumberPartsReturned, int maxNumberPartsReturned) {
+        this.minNumberPartsReturned = minNumberPartsReturned;
+        this.maxNumberPartsReturned = maxNumberPartsReturned;
+    }
+
+    public int howManyPartsToPickUp() {
+        Random random = new Random();
+        return minNumberPartsReturned + random.nextInt(maxNumberPartsReturned);
+    }
+}
